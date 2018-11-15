9b757a90d16b5b5213446732de1cda4182725d15
Daniela Di Iorio <danielad@gmail.com>
2018-11-14 16:36:28 -0800

    fixed line length from linter complaint

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,33 @@
 const Adagrams = {
   drawLetters() {
-    let allLetters = "A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z";
-    allLetters = allLetters.split(" ");
+    let allLetters = [
+      'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+      'B', 'B',
+      'C', 'C',
+      'D', 'D', 'D', 'D',
+      'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+      'F', 'F',
+      'G', 'G', 'G',
+      'H', 'H',
+      'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+      'J',
+      'K',
+      'L', 'L', 'L', 'L',
+      'M', 'M',
+      'N', 'N', 'N', 'N', 'N', 'N',
+      'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+      'P', 'P',
+      'Q',
+      'R', 'R', 'R', 'R', 'R', 'R',
+      'S', 'S', 'S', 'S',
+      'T', 'T', 'T', 'T', 'T', 'T',
+      'U', 'U', 'U', 'U',
+      'V', 'V',
+      'W', 'W',
+      'X',
+      'Y', 'Y',
+      'Z'
+    ];
 
     let indices = [];
     while (indices.length < 10) {
@@ -78,7 +104,7 @@ const Adagrams = {
     });
 
     return {"word": bestWord, "score": bestScore}
-  
+
   },
 };
 
